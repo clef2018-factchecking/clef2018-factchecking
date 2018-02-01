8aac23ba963ac4056dc434929e3afad81f669795
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2018-02-02 01:39:54 +0200

    Removed redundant quotes.

--- scorer/run_scorer.sh
+++ scorer/run_scorer.sh
@@ -6,20 +6,20 @@ gold_file_task1=${PROJ_DIR}/data/gold/'Task1-English-1st-Presidential.txt'
 gold_file_task2=${PROJ_DIR}/data/gold/'Task2-English-1st-Presidential.txt'
 
 echo 'Scoring a random baseline for subtask A'
-python3 subtaskA.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}'subtaskA_random_baseline.txt'
+python3 subtaskA.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}subtaskA_random_baseline.txt
 echo '**********'
 echo 'Scoring the gold predictions for subtask A'
-python3 subtaskA.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}'subtaskA_gold.txt'
+python3 subtaskA.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}subtaskA_gold.txt
 echo '**********'
 echo 'TEST ERROR: Scoring subtask A, where the provided list of line_numbers contains a line_number, which is not in the gold file.'
-python3 subtaskA.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}'subtaskA_other_line_number.txt'
+python3 subtaskA.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}subtaskA_other_line_number.txt
 echo '**********'
 echo '**********'
 echo 'Scoring the gold predictions for subtask B'
-python3 subtaskB.py --gold_file_path=${gold_file_task2} --pred_file=${scorer_tests_path}'subtaskB_gold.txt'
+python3 subtaskB.py --gold_file_path=${gold_file_task2} --pred_file=${scorer_tests_path}subtaskB_gold.txt
 echo '**********'
 echo 'Scoring a random baseline for subtask B'
-python3 subtaskB.py --gold_file_path=${gold_file_task2} --pred_file=${scorer_tests_path}'subtaskB_random_baseline.txt'
+python3 subtaskB.py --gold_file_path=${gold_file_task2} --pred_file=${scorer_tests_path}subtaskB_random_baseline.txt
 echo '**********'
 echo 'TEST ERROR: Scoring a file with predicted labels, which contains a claim_number, which is not present in the gold file.'
-python3 subtaskB.py --gold_file_path=${gold_file_task2} --pred_file=${scorer_tests_path}'/subtaskB_other_claim_number.txt'
+python3 subtaskB.py --gold_file_path=${gold_file_task2} --pred_file=${scorer_tests_path}/subtaskB_other_claim_number.txt
