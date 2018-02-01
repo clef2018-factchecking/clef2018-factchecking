17d19456edeb304f82d938d9ab43f69fe096f1df
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2018-02-02 01:41:27 +0200

    Added tests for the scorer

--- format_checker/tests.py
+++ format_checker/tests.py
@@ -1,32 +1,33 @@
 from unittest import TestCase
-from os.path import join
+from os.path import join, dirname
 
 from format_checker import subtaskA, subtaskB
 
-_EXAMPLES_FOLDER = 'examples/'
+_ROOT_DIR = dirname(dirname(__file__))
+_TEST_DATA_FOLDER = join(_ROOT_DIR, 'data/format_checker_tests/')
 
 
 class FormatCheckerA(TestCase):
-    _OK_FILES = ['subtaskA_OK.txt', 'subtaskA_WARN_MISSING_ID.txt', 'subtaskA_WARN_UNORDERED.txt']
-    _NOT_OK_FILES = ['subtaskA_NOTOK_ALPHA.txt', 'subtaskA_NOTOK_LEADING0.txt', 'subtaskA_NOTOK_0.txt', 'subtaskA_NOTOK_SEP.txt']
+    _OK_FILES = ['subtaskA_OK.txt']
+    _NOT_OK_FILES = ['subtaskA_NOTOK_0.txt', 'subtaskA_NOTOK_MISSING_ID.txt', 'subtaskA_NOTOK_DUP_LINE_NUM.txt']
 
     def test_ok(self):
         for _file in self._OK_FILES:
-            self.assertTrue(subtaskA.check_format(join(_EXAMPLES_FOLDER, _file)))
+            self.assertTrue(subtaskA.check_format(join(_TEST_DATA_FOLDER, _file)))
 
     def test_not_ok(self):
         for _file in self._NOT_OK_FILES:
-            self.assertFalse(subtaskB.check_format(join(_EXAMPLES_FOLDER, _file)))
+            self.assertFalse(subtaskA.check_format(join(_TEST_DATA_FOLDER, _file)))
 
 
 class FormatCheckerB(TestCase):
     _OK_FILES = ['subtaskB_OK.txt','subtaskB_OK_LOWER.txt', 'subtaskB_WARN_MISSING_LABEL.txt']
-    _NOT_OK_FILES = ['subtaskB_NOTOK_OTHER_LABELS.txt', 'subtaskB_NOTOK_MISSING_ID.txt']
+    _NOT_OK_FILES = ['subtaskB_NOTOK_OTHER_LABELS.txt', 'subtaskB_NOTOK_MISSING_ID.txt', 'subtaskB_NOTOK_DUP_CLAIM_NUM.txt']
 
     def test_ok(self):
         for _file in self._OK_FILES:
-            self.assertTrue(subtaskB.check_format(join(_EXAMPLES_FOLDER, _file)))
+            self.assertTrue(subtaskB.check_format(join(_TEST_DATA_FOLDER, _file)))
 
     def test_not_ok(self):
         for _file in self._NOT_OK_FILES:
-            self.assertFalse(subtaskB.check_format(join(_EXAMPLES_FOLDER, _file)))
+            self.assertFalse(subtaskB.check_format(join(_TEST_DATA_FOLDER, _file)))
