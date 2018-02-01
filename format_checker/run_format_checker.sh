94b86c1447b1961ebb6f23b65da557187a17a976
Pepa Gencheva <pepa.k.gencheva@gmail.com>
2018-02-01 10:14:07 +0200

    Refactor names of tests and comments in the running examples script.

--- format_checker/run_format_checker.sh
+++ format_checker/run_format_checker.sh
@@ -2,26 +2,26 @@
 PROJ_DIR="$(dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))"
 format_checker_tests_path=${PROJ_DIR}'/data/format_checker_tests/'
 
-echo '======= This is an example of running the format checker with correct output for Subtask A.'
+echo '======= Subtask A OK: Running the format checker with correct output.'
 python3 subtaskA.py --pred_file_path=${format_checker_tests_path}subtaskA_OK.txt
 echo '======='
-echo '======= This is an example of running the format checker for subtask A, with some missing/skipper line_numbers.'
+echo '======= Subtask A WARNING: Running the format checker with some missing/skipper line_numbers.'
 python3 subtaskA.py --pred_file_path=${format_checker_tests_path}subtaskA_WARN_MISSING_ID.txt
 echo '======='
-echo '======= This is an exmaple of running the format checker for subtask A, where line_numbers are separated with commas, not with new lines.'
+echo '======= Subtask A NOT OK: Running the format checker where line_numbers are separated with commas, not with new lines.'
 python3 subtaskA.py --pred_file_path=${format_checker_tests_path}subtaskA_NOTOK_SEP.txt
 echo '======='
-echo '======= This is an example of running the format checker for subtask A, where the provided list of line_numbers contains duplicates.'
-python3 subtaskA.py --pred_file_path=${format_checker_tests_path}'subtaskA_dup_line_number.txt'
+echo '======= Subtask A NOT OK: Running the format checker where the provided list of line_numbers contains duplicates.'
+python3 subtaskA.py --pred_file_path=${format_checker_tests_path}'subtaskA_NOTOK_dup_line_number.txt'
 echo '======='
-echo '======= This is an example of running the format checker with correct output for Subtask B.'
+echo '======= Subtask B OK: Running the format checker with correct output.'
 python3 subtaskB.py --pred_file_path=${format_checker_tests_path}subtaskB_OK.txt
 echo '======='
-echo '======= This is an example of running the format checker on file with predicted labels, where the labels are different from TRUE, FALSE, HALF-TRUE.'
+echo '======= Subtask B NOT OK: Running the format checker where the labels are different from TRUE, FALSE, HALF-TRUE.'
 python3 subtaskB.py --pred_file_path=${format_checker_tests_path}subtaskB_NOTOK_OTHER_LABELS.txt
 echo '======='
-echo '======= This is an example of running the format checker on file with predicted labels, where some claim_numbers are missing from the file.'
+echo '======= Subtask B WARNING: Running the format checker where some claim_numbers are missing from the file.'
 python3 subtaskB.py --pred_file_path=${format_checker_tests_path}subtaskB_WARN_MISSING_LABEL.txt
 echo '======='
-echo '======= This is an example of running the format checker for subtask A, where the provided predictions contains duplicate claim_numners with different labels.'
-python3 subtaskB.py --pred_file_path=${format_checker_tests_path}subtaskB_dup_claim_number.txt
+echo '======= Subtask B NOT OK: Running the format checker where the provided predictions contains duplicate claim_numbers with different labels.'
+python3 subtaskB.py --pred_file_path=${format_checker_tests_path}subtaskB_NOTOK_dup_claim_number.txt
