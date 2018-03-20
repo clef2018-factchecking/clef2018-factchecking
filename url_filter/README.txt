0069eab9374080a8204a03d7b7fc471a62e2c82e
Alberto Barron <albarron@gmail.com>
2018-03-20 11:49:57 +0300

    adding the URL filter

---
+++ url_filter/README.txt
@@ -0,0 +1,14 @@
+URL filter for the CLEF-2018 Fact Checking Lab.
+
+Works with both Python 2 (2.5+) and Python 3.
+Use the provided "is_url_bad_task1" and "is_url_bad_task2" functions to check if a URL is not good for usage in tasks 1 or 2 of the lab respectively. They expect a single parameter - the URL to test, represented as a string, and return a boolean value.
+
+The filter checks a number of matchers against the lowercased version of an input URL string.
+If all items from a single line in the matchers array are contained within the URL it is deemed as not good for usage.
+For task1 the matchers array is MATCHERS_ALL, for task2 - MATCHERS_SIMPLE.
+
+In order to handle shortened URLs, the filtering functions attempt to expand all URLs to their full form (needs active internet connectivity) and then follow the same matching procedure as above using the extended form.
+An error in the URL expansion process will mark the input URL as not good.
+
+The python file contains some examples, simply run it with "python url_filter.py" and observe the output.
+
---
+++ url_filter/url_filter.py
@@ -0,0 +1,156 @@
+# URL filter for the CLEF-2018 Fact Checking Lab
+
+import sys
+if sys.version_info[0] < 3:
+	import urllib2 as urllib
+	import cookielib as cookielib
+else:
+	import urllib.request as urllib
+	import http.cookiejar as cookielib
+
+# Impersonate some real user agent so less network requests fail (URL expansion)
+_USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0'
+
+# Each item is tested for containment within the lower cased URL
+# Logical AND for items in a single row
+MATCHERS_ALL = [
+	# Fact-checking sites US
+	['factcheck.org'],
+	['politifact'],
+	['snopes'],
+	['truthorfiction'],
+	['climatefeedback'],
+	['gossipcop'],
+	# Sites with fact-checking sections US
+	['apnews', 'not-real-news'],
+	['apnews', 'fact-check'],
+	['washingtonpost', 'fact-check'],
+	['azcentral', 'fact-check'],
+	['cbslocal', 'reality-check'],
+	['thenevadaindependent', 'fact-check'],
+	['thegazette', 'fact-check'],
+	['thegazette', 'factchecker'],
+	['nytimes', 'fact-check'],
+	['bridgemi', 'michigan-truth-squad'],
+	['channel3000', 'reality-check'],
+	['kmov', 'fact-check'],
+	['npr', 'fact-check'],
+	['qctimes', 'fact-check'],
+	['politico', 'fact-check'],
+	['weeklystandard', 'fact-check'],
+	['ballotpedia', 'fact_check'],
+	['wral', 'fact-check'],
+	['abcnews', 'fact-check'],
+	['chicagotribune', 'fact-check'],
+	['cnn', 'fact-check'],
+	['theguardian', 'fact-check'],
+	['usatoday', 'fact-check'],
+	# UK & other
+	['hoax-slayer'],
+	['fullfact'],
+	['factcheckni'],
+	['theconversation', 'fact-check'],
+	['bbc', 'realitycheck'],
+	['bbc', 'reality-check'],
+	['channel4', 'factcheck'],
+	['theferret', 'fact-check'],
+	['theferret', 'fact-service'],
+	['abc', 'fact-check'],
+	['pbs', 'fact-check'],
+	['foxnews', 'fact-check'],
+	# Possibly Arabic
+	['factnameh'],
+	['rouhanimeter'],
+	['thewhistle'],
+	['morsimeter'],
+	['larbitrefact'],
+	['meter.iwatch'],
+	['sebsimeter'],
+	['jomaameter'],
+	['essidmeter'],
+	# Special phrases English
+	['debate-highlighted'],
+	['debate-transcript-annotated'],
+	['fact check'],
+	['fact%20check'],
+	['fact+check'],
+	['fact_check'],
+	['fact-check'],
+	['factcheck'],
+	['reality check'],
+	['reality%20check'],
+	['reality+check'],
+	['reality_check'],
+	['reality-check'],
+	['realitycheck'],
+	['fake news'],
+	['fake%20news'],
+	['fake+news'],
+	['fake_news'],
+	['fake-news'],
+	['fakenews']
+]
+
+MATCHERS_SIMPLE = [
+	['factcheck'],
+]
+
+# Makes an HTTP HEAD request to the provied url and then returns the possibly expanded URL from the response
+def _expand_url(url):
+	request = urllib.Request(url)
+	request.get_method = lambda : 'HEAD'
+	request.add_header('User-Agent', _USER_AGENT)
+	cj = cookielib.CookieJar()
+	opener = urllib.build_opener(urllib.HTTPCookieProcessor(cj))
+	response = opener.open(request)
+	return response.geturl()
+
+def _check_matchers(urlString, allMatchers = True):
+	if not isinstance(urlString, str):
+		print('Error, the provided parameter is not a string. Will be marked as BAD.')
+		return True
+	urlLower = urlString.lower()
+	matchers = MATCHERS_ALL if allMatchers else MATCHERS_SIMPLE
+	return any([all([item in urlLower for item in row]) for row in matchers])
+
+def _is_url_bad(urlString, allMatchers = True):
+	if _check_matchers(urlString, allMatchers):
+		return True
+	try:
+		urlExpanded = _expand_url(urlString)
+	except:
+		print('Error while expanding URL "' + urlString + '":', str(sys.exc_info()[1]))
+		print('Will be marked as BAD')
+		return True
+	return _check_matchers(urlExpanded, allMatchers)
+
+def is_url_bad_task1(urlString):
+	return _is_url_bad(urlString)
+
+def is_url_bad_task2(urlString):
+	return _is_url_bad(urlString, False)
+
+EXAMPLES = [
+	# BAD for both tasks:
+	'https://www.factcheck.org/2018/03/trump-veterans-choice-program/',
+	'https://www.channel4.com/news/factcheck/ann-coulters-10-misleading-claims-about-immigrants-and-mass-shootings',
+	'http://bit.ly/2IA2znR',
+	# BAD for task1, OK for task2:
+	'https://www.washingtonpost.com/news/fact-checker/wp/2018/02/18/fact-checking-trumps-error-filled-tweet-storm-about-the-russia-investigation/',
+	'http://www.thegazette.com/subject/news/government/fact-check/fact-checking-the-speech-trump-plays-loose-with-facts-20170621',
+	'https://twitter.com/snopes/status/963534404047048710',
+	'http://bit.ly/2CMmrAl',
+	# OK for both tasks:
+	'https://www.washingtonpost.com/news/tripping/wp/2017/11/30/car-rental-companies-find-a-way-to-ding-motorists-for-electronic-tolling/',
+	'https://en.wikipedia.org/wiki/Donald_Trump',
+	'http://goo.gl/eRJPti',
+	# BAD for technical reasons:
+	1616,
+	'http://www.not-a-real-site-and-so-cannot-be-expanded.com'
+]
+
+if __name__ == '__main__':
+	for site in EXAMPLES:
+		labelTask1 = 'BAD' if is_url_bad_task1(site) else 'OK '
+		labelTask2 = 'BAD' if is_url_bad_task2(site) else 'OK '
+		print('task1: ' + labelTask1, 'task2: ' + labelTask2, site)
