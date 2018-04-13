8899a33860e83c76399ac43a520327d64eee0735
Spas Kyuchukov <2a7acb9a@opayq.com>
2018-04-13 22:11:26 +0300

    Update run_scorer.sh and run_scorer_out.txt with multiple file scoring

--- scorer/run_scorer.sh
+++ scorer/run_scorer.sh
@@ -11,6 +11,9 @@ echo '**********'
 echo 'Scoring the gold predictions for task 1'
 python3 task1.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}task1_gold.txt
 echo '**********'
+echo 'Scoring BOTH the gold and random baseline for task 1 (only for example purposes, scoring multiple files for 1 debate will lead to wrong metrics)'
+python3 task1.py --gold_file_path="${gold_file_task1}, ${gold_file_task1}" --pred_file_path="${scorer_tests_path}task1_gold.txt, ${scorer_tests_path}task1_random_baseline.txt"
+echo '**********'
 echo 'TEST ERROR: Scoring task 1, where the provided list of line_numbers contains a line_number, which is not in the gold file.'
 python3 task1.py --gold_file_path=${gold_file_task1} --pred_file_path=${scorer_tests_path}task1_other_line_number.txt
 echo '**********'
@@ -23,6 +26,9 @@ python3 task2.py --gold_file_path=${gold_file_task2} --pred_file_path=${scorer_t
 echo '**********'
 echo 'Scoring a random baseline for task 2'
 python3 task2.py --gold_file_path=${gold_file_task2} --pred_file_path=${scorer_tests_path}task2_random_baseline.txt
+echo '**********'
+echo 'Scoring BOTH the gold and random baseline for task 2 (only for example purposes, scoring multiple files for 1 debate will lead to wrong metrics)'
+python3 task2.py --gold_file_path="${gold_file_task2}, ${gold_file_task2}" --pred_file_path="${scorer_tests_path}task2_gold.txt, ${scorer_tests_path}task2_random_baseline.txt"
 echo '**********'
 echo 'TEST ERROR: Scoring task 2, with predictions that contains a claim_number, which is not present in the gold file.'
 python3 task2.py --gold_file_path=${gold_file_task2} --pred_file_path=${scorer_tests_path}task2_other_claim_number.txt
